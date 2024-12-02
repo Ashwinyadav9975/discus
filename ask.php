afbc0367da326aef0b2842db0119f0eeeda82577
Ashwin yadav <95066620+Ashwinyadav9975@users.noreply.github.com>
2024-12-02 10:05:42 +0530

    Add files via upload

---
+++ ask.php
@@ -0,0 +1,17 @@
+<div class="container">
+    <h1 class="heading">ask the question</h1>
+<form action="./server/requests.php" method="post">
+  
+  <div class="col-6 offset-sm-3">
+    <label for="title" class="form-label" > title</label>
+    <input type="text" name="title" class="form-control" id="title" placeholder="enter question" >
+  </div>
+ 
+  <div class="col-6 offset-sm-3">
+  <button type="submit" name="login" class="btn btn-primary">submit</button>
+  </div>
+</form>
+
+
+
+</div>
